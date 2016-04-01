e16c66e3caa96b8c4276a009da8be16b59a2207c
Haroon Ahmed <hrahmed@gmail.com>
2016-04-01 17:40:02 -0400

    end to end with MathApp Working

--- app/ops/simpleOpsAsync.js
+++ app/ops/simpleOpsAsync.js
@@ -9,31 +9,55 @@ module.exports = {
     add: function (value1, value2, callback) {
         process.nextTick(function(){
             var result = mathops.eval(value1 + value2);
-            callback(result);
+            process.nextTick(function(){
+                setTimeout(function() {
+                    callback(result);
+                }, 2000);
+            });
         });
     },
     subtract: function (value1, value2, callback) {
         process.nextTick(function(){
             var result = mathops.eval(value1 - value2);
-            callback(result);
+            process.nextTick(function(){
+                setTimeout(function() {
+                    callback(result);
+                }, 2000);
+
+            });
         });
     },
     multiply: function (value1, value2, callback) {
         process.nextTick(function(){
             var result = mathops.eval(value1 * value2);
-            callback(result);
+            process.nextTick(function(){
+                setTimeout(function() {
+                    callback(result);
+                }, 2000);
+
+            });
         });
     },
     divide: function (value1, value2, callback) {
         process.nextTick(function(){
             var result = mathops.eval(value1 / value2);
-            callback(result);
+            process.nextTick(function(){
+                setTimeout(function() {
+                    callback(result);
+                }, 2000);
+
+            });
         });
     },
     sqrt: function (value1, callback) {
         process.nextTick(function(){
             var result = mathops.sqrt(value1);
-            callback(result);
+            process.nextTick(function(){
+                setTimeout(function() {
+                    callback(result);
+                }, 2000);
+
+            });
         });
     }
 };
